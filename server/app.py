70f51312f0cdbcc37dca29186244a32022ee3bd1
Saraanssh1905 <mehrasaraanssh@gmail.com>
2026-03-31 21:09:37 +0530

    Changed dependencies in requirements.txt in root repo and server/

--- server/app.py
+++ server/app.py
@@ -7,11 +7,14 @@ app = create_fastapi_app(
     TriageAction,
     TriageObservation
 )
+from fastapi.responses import RedirectResponse
 
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
 def main():
     import uvicorn
-    uvicorn.run("server.app:app", host="0.0.0.0", port=8000)
-
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
 
 if __name__ == "__main__":
     main()
