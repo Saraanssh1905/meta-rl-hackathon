98f920a7539d39380149ec282b77b39ebdc7b2c8
Saraanssh1905 <mehrasaraanssh@gmail.com>
2026-04-12 12:29:28 +0530

    changes in app.py

--- server/app.py
+++ server/app.py
@@ -1,17 +1,12 @@
 from openenv.core.env_server import create_fastapi_app
 from server.environment import IncidentTriageEnvironment
 from models import TriageAction, TriageObservation
-from fastapi.responses import RedirectResponse
 
 app = create_fastapi_app(
     IncidentTriageEnvironment,
     TriageAction,
     TriageObservation
 )
-
-@app.get("/")
-async def root():
-    return RedirectResponse(url="/docs")
 
 #  Used by OpenEnv
 def main():
